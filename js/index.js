6ea2e7f7cf46b1e456dd622c7a2652e297df1b0d
demina-ivan23 <demina.ivann@gmail.com>
2024-08-14 13:51:31 +0300

    dynamic page rendering and links toggle

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const toggleActiveLinks = () => {
+    let hash = window.location.hash || '#home';
+    let links = document.querySelectorAll('.header__navbar__link-list__element__link');
+    let activeLink = document.querySelector(`${hash}-link`);
+    links.forEach(link => {
+        link.classList.remove('active'); 
+    });
+    if (activeLink) {
+        activeLink.classList.add('active');
+    }
+};
+
+window.addEventListener('hashchange', (event) => {
+    toggleActiveLinks();
+});
+
+toggleActiveLinks();
