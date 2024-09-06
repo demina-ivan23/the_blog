f2fa399c710021728f67a59e1ca154fc4da8c8b8
demina-ivan23 <demina.ivann@gmail.com>
2024-09-06 14:12:22 +0300

    fix(ui): minor theme toggle fix

--- js/index.js
+++ js/index.js
@@ -5,12 +5,21 @@ const setTheme = () => {
     if (darkThemeOn == "true") {
         document.getElementById('main').classList.add('dark-theme');
         document.getElementById('theme-toggle').checked = true;
+        document.getElementById('theme-toggle-burger').checked = true;
     } else {
         document.getElementById('main').classList.remove('dark-theme');
         document.getElementById('theme-toggle').checked = false;
+        document.getElementById('theme-toggle-burger').checked = false;
+
     }
 }
-const toggleTheme = () => {
+const toggleTheme = (targetId) => {
+    console.log(targetId);
+    if(targetId == 'theme-toggle'){
+        document.getElementById('theme-toggle-burger').checked = !document.getElementById('theme-toggle-burger').checked 
+    } else if(targetId == 'theme-toggle-burger'){
+        document.getElementById('theme-toggle').checked = !document.getElementById('theme-toggle').checked 
+    }
     document.getElementById('main').classList.toggle('dark-theme');
     sessionStorage.setItem('darkThemeOn', document.getElementById('main').classList.contains('dark-theme'));
 }
@@ -48,7 +57,8 @@ const closeMenu = () => {
 //Code execution and listeners attachment
 
 setTheme();
-document.getElementById('theme-toggle').addEventListener('click', toggleTheme);
+document.getElementById('theme-toggle').addEventListener('click', (event) => { toggleTheme(event.target.id) });
+document.getElementById('theme-toggle-burger').addEventListener('click', (event) => { toggleTheme(event.target.id) });
 
 
 window.addEventListener('hashchange', (event) => {
@@ -60,6 +70,6 @@ keepMenuOpen();
 document.getElementById('burger-open').addEventListener('click', openMenu);
 document.getElementById('burger-close').addEventListener('click', closeMenu);
 const links = document.querySelectorAll('.header__navbar__link-list__element__link');
-for(let link of links){
+for (let link of links) {
     link.addEventListener('click', closeMenu);
 }
